eb136c4d6386a8cd68c8859bda2030327d114169
kem <70446293+Karim-Mammadli@users.noreply.github.com>
2022-05-07 22:44:31 -0400

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,118 +1,34 @@
-/**
- * CS 251: Data Structures and Algorithms
- * Project 3: Part 1
- *
- * TODO: implement the sorting methods.
- *
- * @author TODO: add your name here
- * @username TODO: add your Purdue username here
- * @sources TODO: list your sources here
- *
- * DO NOT COPY PASTE THE ALGORITHMS FOR SORTING METHODS FROM THE INTERNET. WE WILL KNOW!
- *
- */
-
 import java.util.ArrayList;
 
 public class Sorting<Item extends Comparable<Item>> {
 
-
-    /**
-     *
-     * Default Constructor
-     *
-     */
     public Sorting() {
     }
-
-    /**
-     *
-     * returns true if the Item at index @param i in @ param list is greater than Item at index @param j
-     * else return false
-     *
-     * @param list
-     * @param i
-     * @param j
-     * @return
-     */
+    
     public boolean greaterThan(ArrayList<Item> list, int i, int j) {
         return list.get(i).compareTo(list.get(j)) > 0;
     }
 
-    /**
-     *
-     * returns true if the Item @param i is greater than Item @param j else return false
-     *
-     * @param i
-     * @param j
-     * @return
-     */
     public boolean greaterThan(Item i, Item j) {
         return i.compareTo(j) > 0;
     }
 
-
-    /**
-     *
-     * returns true if the Item at index @param i in @ param list is lesser than Item at index @param j
-     * else return false
-     *
-     * @param list
-     * @param i
-     * @param j
-     * @return
-     */
     public boolean lessThan(ArrayList<Item> list, int i, int j) {
         return list.get(i).compareTo(list.get(j)) < 0;
     }
-
-    /**
-     *
-     * returns true if the Item @param i is less than Item @param j else return false
-     *
-     * @param i
-     * @param j
-     * @return
-     */
+    
     public boolean lessThan(Item i, Item j) {
         return i.compareTo(j) < 0;
     }
 
-
-    /**
-     *
-     * returns true if the Item @param i is equal to Item @param j else return false
-     *
-     * @param i
-     * @param j
-     * @return
-     */
     public boolean equal(Item i, Item j) {
         return i.compareTo(j) == 0;
     }
 
-
-    /**
-     *
-     * returns true if the Item at index @param i in @ param list is equal to the Item at index @param j
-     * else return false
-     *
-     * @param list
-     * @param i
-     * @param j
-     * @return
-     */
     public boolean equal(ArrayList<Item> list, int i, int j) {
         return list.get(i).compareTo(list.get(j)) == 0;
     }
 
-
-    /**
-     * Sorts the list in ascending order using Insertion Sort.
-     *
-     *
-     * @param list
-     */
     public ArrayList<Item> insertionSort(ArrayList<Item> list) {
         int index = 1;
         int swap_index = -1;
@@ -143,14 +59,6 @@ public class Sorting<Item extends Comparable<Item>> {
         return list;
 
     }
-
-
-    /**
-     *
-     * Sorts the list in ascending order using Merge Sort.
-     *
-     * @param list
-     */
 
     public ArrayList<Item> mergeSort(ArrayList<Item> list) {
 
@@ -215,13 +123,6 @@ public class Sorting<Item extends Comparable<Item>> {
 
     }
 
-
-    /**
-     *
-     * prints the Items in the @param list.
-     *
-     * @param list
-     */
     public void print(ArrayList<Item> list) {
         int n = list.size();
 
@@ -235,27 +136,13 @@ public class Sorting<Item extends Comparable<Item>> {
         System.out.println(bf.toString());
     }
 
-
-    /**
-     *
-     * Swaps the element at index @param i with element at index @param j in the @param list.
-     *
-     * @param list
-     * @param i
-     * @param j
-     */
     private void swap(ArrayList<Item> list, int i, int j) {
         Item temp = list.get(i);
         list.set(i, list.get(j));
         list.set(j, temp);
     }
 
-    /**
-     *
-     * This is the main function to run the Sorting class to help with debugging.
-     *
-     * @param args
-     */
+
     public static void main(String[] args) {
 
         Sorting<Integer> s = new Sorting<>();
